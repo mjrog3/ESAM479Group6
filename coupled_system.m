b9962bf0a01481c850ceac4abe0455cc84378ef0
mjrog3 <maxrogal2024@u.northwestern.edu>
2024-04-22 10:11:27 +0300

    added function for general coupled system

--- coupled_system.m
+++ coupled_system.m
@@ -1,12 +1,14 @@
 function data = coupled_system(init_cond, N, rx, ry, betaxy, betayx)
 % general function for coupled system, based on Sugihara equation (1)
-% for asymmetric case, betaxy=0
-    data = NaN(N,2);
-    data(1,:) = init_cond;
+% for asymmetric case: 
+% rx = 3.7; ry = 3.7; betayx = .32; betaxy=0
+% init_cond = (0.2,0.4);
 
-    for ii=1:(N-1)
-        data(ii+1,:) = step(data(ii,:),rx,ry,betaxy,betayx);
-    end
+data = NaN(N,2);
+data(1,:) = init_cond;
+for ii=1:(N-1)
+    data(ii+1,:) = step(data(ii,:),rx,ry,betaxy,betayx);
+end
 end
 
 function Y_out = step(data, rx, ry, betaxy, betayx)
