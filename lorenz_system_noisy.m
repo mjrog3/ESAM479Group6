70dbbd96e020c382fafda65fed48eb35aea0d25b
mjrog3 <maxrogal2024@u.northwestern.edu>
2024-04-25 17:17:53 +0300

    updated noisy lorenz

--- lorenz_system_noisy.m
+++ lorenz_system_noisy.m
@@ -1,40 +1,19 @@
-function lorenz_system_noisy()
-    sigma = 10;
-    b = 8/3;
-    r = 28; % Typical value for observing chaos
-    initial_conditions = [1 1 1; 1.01 1 1]; % Initial conditions [x0; y0; z0]
-    tspan = [0 10]; % Extended time span for the simulation
+function out = generate_noisy_lorenz(sigma, b, r, IC, tspan)
+    % sigma = 10;
+    % b = 8/3;
+    % r = 28; % Typical value for observing chaos
+    % initial_conditions = [1 1 1; 1.01 1 1]; % Initial conditions [x0; y0; z0]
+    % tspan = [0 10]; % Extended time span for the simulation
     noise_levels = [1 1 1]; % x y and z noise levels
 
     opts = odeset('RelTol',1e-5,'AbsTol',1e-8); % Setting solver options for better accuracy
 
-    for i = 1:2
-        [t, sol] = ode45(@(t, y) lorenzODE(t, y, sigma, b, r, noise_levels), tspan, initial_conditions(i,:), opts);
-        x = sol(:, 1);
-        y = sol(:, 2);
-        z = sol(:, 3);
+    [t, sol] = ode45(@(t, y) lorenzODE(t, y, sigma, b, r), tspan, IC, opts);
+    x = sol(:, 1);
+    y = sol(:, 2);
+    z = sol(:, 3);
 
-        figure; % Create a new figure window
-        subplot(3, 1, 1);
-        plot(t, x);
-        title(['x(t) with r = ', num2str(r)]);
-        xlabel('Time');
-        ylabel('x');
-
-        subplot(3, 1, 2);
-        plot(t, y);
-        title(['y(t) with r = ', num2str(r)]);
-        xlabel('Time');
-        ylabel('y');
-
-        subplot(3, 1, 3);
-        plot3(x, y, z);
-        title('3D Trajectory of Lorenz Attractor');
-        xlabel('x');
-        ylabel('y');
-        zlabel('z');
-        grid on; % Enable grid for better visualization
-    end
+    out = [t x y z];
 end
 
 function dy = lorenzODE(t, y, sigma, b, r, noise_levels)
